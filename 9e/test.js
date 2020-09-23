16b92ce9ca7dc14ea1b62eab7aedf6758af4d4e2
metalyx <metalyx3@gmail.com>
2020-09-23 15:22:47 +0300

    c
    1

---
+++ 9e/test.js
@@ -0,0 +1,56 @@
+module.exports = {
+
+    sub : [],
+    /**
+     * @param {String} event
+     * @param {Object} subscriber
+     * @param {Function} handler
+     */
+    on: function (event, subscriber, handler) {
+        if (this.sub.indexOf(subscriber) == -1)
+        {
+            this.sub.push(subscriber);
+        }
+
+        var keys = Object.keys(subscriber);
+
+        if (keys.indexOf(event) == -1)
+        {
+            subscriber[event] = [];
+        }
+
+        subscriber[event].push(handler);
+        return this;
+
+
+    },
+
+    /**
+     * @param {String} event
+     * @param {Object} subscriber
+     */
+    off: function (event, subscriber) {
+       if (this.sub.indexOf(subscriber) != -1)
+       {
+           delete this.sub[this.sub.indexOf(subscriber)][event];
+       }
+       return this;
+    },
+
+    /**
+     * @param {String} event
+     */
+    emit: function (event) {
+        this.sub.map(function(s){
+            if(s[event])
+            {
+                var handlers = s[event];
+                for (var i = 0; i < handlers.length; i++)
+                {
+                    handlers[i].apply(s);
+                }
+            }
+        })
+        return this;
+    }
+};
